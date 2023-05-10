36c4f2575c09e54787a45b5b09a13e4d65e6ffe2
FRAALES <alessandrofranco1992@gmail.com>
2023-05-10 16:05:00 +0200

    bloccatissimo a far apparire la griglia

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Definisci la variabile per il contenitore della griglia che creerai nel prossimo passaggio
+let creaGriglia = document.getElementById('grid-container');
+console.log(creaGriglia);
+
+//Creo la griglia in un div a cui ho già dato id nell'html
+let grigliaVeraEPropria = document.createElement('div');
+grigliaVeraEPropria.innerHTML = '<div id = grid></div>';
+
+
+//definisci la variabile per il bottone che farà apparire la griglia
+let bottoneCompareGriglia = document.getElementById('btn');
+
+//Dì al bottone che quando lo clicchi deve far apparire la griglia
+bottoneCompareGriglia.addEventListener('click', grigliaVeraEPropria);
